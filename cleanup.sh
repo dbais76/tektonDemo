5fd83ed81f4de35833a9254f8d7e299df77fea3e
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2023-10-14 10:32:04 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+oc get tasks
+echo "Deleting tasks "
+oc delete tasks
 oc get taskruns 
+echo "Deleting taskruns "
 oc delete taskruns --all
 oc get pipelinerun
+echo "Deleting pipelineruns "
 oc delete pipelinerun --all
+echo "Deleting Pipeline now "
 oc delete pipeline --all
